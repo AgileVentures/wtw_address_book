1f8899d3d032cd49bbef09518020f42c7820b55f
Thomas <thomas@craftacademy.se>
2021-09-01 21:30:24 +0200

    adds AddressBook#create

--- specs/units/AddressBook.spec.js
+++ specs/units/AddressBook.spec.js
@@ -1,15 +1,39 @@
+const AddressBook = require("../../src/js/AddressBook");
 
-const AddressBook = require('../../src/js/AddressBook')
+describe("AddressBook", () => {
+  subject(() => new AddressBook());
 
-describe('AddressBook', () => {
-  subject(()=> new AddressBook())
+  it(() => is.expected.to.be.an("object"));
+  it(() => is.expected.to.be.an.instanceof(AddressBook));
 
-  it('is expected to be an Object', () => {
-    expect($subject).to.be.a('object')
-  })
+  describe.only("#create", () => {
+    it(() => is.expected.to.respondTo("create"));
+    let setItemSpy;
+    def("validData", {
+      name: "Thomas",
+      email: "thomas@whatever.com",
+      phone: "031-111111",
+    });
+    before(() => {
+      setItemSpy = sinon.spy(window.localStorage, "setItem");
+    });
 
-  it('is expected to be an instance of AddressBook', () => {
-    expect($subject).to.be.instanceof(AddressBook)
-  })
-  
+    context("with valid data/attributes", () => {
+      def("response", () => $subject.create($validData));
+      def("contactsInStorage", () =>
+        JSON.parse(window.localStorage.getItem("contacts"))
+      );
+      beforeEach(() => {
+        $response;
+      });
+
+      it("is expected to make use of localStorage.setItem", () => {
+        expect(setItemSpy).to.have.been.calledOnce;
+      });
+
+      it("is expected to store a contact", () => {
+        expect($contactsInStorage).to.have.length(1);
+      });
+    });
+  });
 });
--- src/js/AddressBook.js
+++ src/js/AddressBook.js
@@ -1,4 +1,11 @@
-class AddressBook {}
+class AddressBook {
+  create(data) {
+    let contacts = []
+    contacts.push(data)
+    window.localStorage.setItem('contacts', JSON.stringify(contacts))
+    return "Yay!"
+  }
+}
 
 
 if (typeof module !== "undefined" && module.exports) {
